89a70024aef65496c87e4c443083dae959f8bf5e
KevinLubbers <kelubbers@gmail.com>
2024-04-25 17:44:59 -0400

    testing prevent default

--- js.js
+++ js.js
@@ -1,31 +1,27 @@
 const colorBox = document.getElementById('mainStage');
 
 function changeColor(){
+	document.documentElement.requestFullscreen();
 	const red = Math.random() * 250;
 	const green = Math.random() * 250;
 	const blue = Math.random() * 250;
 
 	colorBox.style.backgroundColor = "rgb("+red+"," +blue+ "," +green+")";
 }
-function lockScreen(){
-	document.documentElement.requestFullscreen();
 
-}
 function unlockScreen(){
 	document.exitFullscreen();
 }
 document.addEventListener('touchstart', changeColor);
 document.addEventListener('touchcancel', changeColor);
 document.addEventListener('touchend', function(event){
-	event.preventDefault();
 });
 document.addEventListener('touchmove', changeColor);
 document.addEventListener('scroll', function(event){
-	event.preventDefault();
+	
 });
 document.addEventListener('click', changeColor);
 document.addEventListener('pointerdown', function(event){
-	event.preventDefault();
 });
 document.addEventListener('pointerup', function(event){
 	event.preventDefault();
