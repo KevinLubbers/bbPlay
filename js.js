ac54d301836a3834e6afe3423ff96cda21c37ff4
KevinLubbers <kelubbers@gmail.com>
2024-04-25 17:47:50 -0400

    revert

--- js.js
+++ js.js
@@ -1,14 +1,16 @@
 const colorBox = document.getElementById('mainStage');
 
 function changeColor(){
-	document.documentElement.requestFullscreen();
 	const red = Math.random() * 250;
 	const green = Math.random() * 250;
 	const blue = Math.random() * 250;
 
 	colorBox.style.backgroundColor = "rgb("+red+"," +blue+ "," +green+")";
 }
+function lockScreen(){
+	document.documentElement.requestFullscreen();
 
+}
 function unlockScreen(){
 	document.exitFullscreen();
 }
