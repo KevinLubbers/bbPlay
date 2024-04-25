df2eab074b8b6f1643b83961989af4124c430085
KevinLubbers <kelubbers@gmail.com>
2024-04-25 16:26:06 -0400

    small mistake fixed

--- js.js
+++ js.js
@@ -2,9 +2,9 @@ const colorBox = document.getElementById('contentWindow');
 
 function changeColor(){
 	event.preventDefault();
-	const red = Math.random() * 100;
-	const green = Math.random() * 100;
-	const blue = Math.random() * 100;
+	const red = Math.random() * 250;
+	const green = Math.random() * 250;
+	const blue = Math.random() * 250;
 
 	colorBox.style.backgroundColor = "rgb("+red+"," +blue+ "," +green+")";
 }
