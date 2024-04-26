f3666b17069a7889dd5cd1a43dec4b14c9251b53
KevinLubbers <kelubbers@gmail.com>
2024-04-26 17:20:34 -0400

    color change on touchstart and end, colored circles on touchmove

--- js.js
+++ js.js
@@ -3,7 +3,7 @@ const eventBox = document.getElementById('eventBox');
 let canChangeColor = true;
 
 function changeColor(eventType){
-	if (canChangeColor){
+	if (canChangeColor || eventType === 'touchend') {
 		
 		const red = Math.random() * 250;
 		const green = Math.random() * 250;
@@ -15,8 +15,35 @@ function changeColor(eventType){
 		canChangeColor = false;
 		setTimeout(function(){
 			canChangeColor = true;
-		}, 2000);
+		}, 3000);
 	}
+}
+function moveColor(){
+
+}
+function createColorDot(e) {
+	var x = e.pageX || e.touches[0].pageX;
+	var y = e.pageY || e.touches[0].pageY;
+	const red = Math.random() * 250;
+	const green = Math.random() * 250;
+	const blue = Math.random() * 250;
+
+	const dot = document.createElement('div');
+	dot.className = 'color-dot';
+	dot.style.backgroundColor = "rgb("+red+","+blue+","+green+")"; 
+	dot.style.left = x + 'px';
+	dot.style.top = y + 'px';
+	colorBox.appendChild(dot);
+	fadeOut(dot);
+
+	eventBox.innerHTML = "Event: " + e.type; 
+}
+
+function fadeOut(element) {
+	setTimeout(function() {
+		element.style.opacity = 0;
+		setTimeout(function() { colorBox.removeChild(element); }, 1500);
+	}, 500);
 }
 function lockScreen(){
 	document.documentElement.requestFullscreen();
@@ -27,14 +54,12 @@ function unlockScreen(){
 }
 document.addEventListener('touchstart', () => changeColor('touchstart'));
 document.addEventListener('touchcancel', () => changeColor('touchcancel'));
-document.addEventListener('touchend', () => changeColor('touchend'));
-document.addEventListener('touchmove', () => changeColor('touchmove'));
+document.addEventListener('touchend', () => changeColor('touchend')); 
+document.addEventListener('touchmove', function(e) { createColorDot(e); });
 document.addEventListener('scroll', () => changeColor('scroll'));
 
 document.addEventListener('click', () => changeColor('click'));
-document.addEventListener('pointerdown', () => changeColor('pointerdown'));
-document.addEventListener('pointerup', () => {
-    changeColor('pointerup');
-});
+document.addEventListener('pointerdown', () => function(e) { createColorDot(e); });
+document.addEventListener('pointerup', () => function(e) { createColorDot(e); });
 
 
